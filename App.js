cc603e1a6f4e753c69b871a1b8090ec71fe862d8
Thais RIbeiro <thaisribeiro.mesquita@gmail.com>
2020-07-09 23:46:16 -0300

    Adicionado projeto com demonstração de uso de flexbox

--- App.js
+++ App.js
@@ -5,8 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View style={styles.box1}></View>
+     <View style={styles.box2}></View>
+     <View style={styles.box3}>
+      <View style={styles.subbox}></View>
+      <View style={styles.subbox}></View>
+      <View style={styles.subbox}></View>
+     </View>
     </View>
   );
 }
@@ -15,7 +20,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
+    flexDirection:'column'
   },
+  box1: {
+    flex: 1,
+    backgroundColor:'blue',
+    margin: 4
+  },
+  box2: {
+    flex: 2,
+    backgroundColor:'blue',
+    margin: 4
+  },
+  box3: {
+    flex: 3,
+    backgroundColor:'blue',
+    margin: 4,
+    flexDirection:'row'
+  },
+  subbox: {
+    flex: 1,
+    margin: 4,
+    backgroundColor:'yellow'
+  }
 });
